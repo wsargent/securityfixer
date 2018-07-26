13ddb6c3857a07bc226334a361b9a1a0182f15a2
Will Sargent <will.sargent@gmail.com>
2018-07-26 10:16:05 -0700

    Update script to run from JAR

--- agent/src/main/java/com/tersesystems/securityfixer/agent/SecurityFixerAgent.java
+++ agent/src/main/java/com/tersesystems/securityfixer/agent/SecurityFixerAgent.java
@@ -5,10 +5,13 @@ import com.tersesystems.securityfixer.bootstrap.MySystemInterceptor;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.description.NamedElement;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.implementation.Implementation;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.matcher.ElementMatcher;
 import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.utility.JavaModule;
 
 import java.io.File;
 import java.io.IOException;
@@ -87,16 +90,21 @@ public class SecurityFixerAgent {
         // And then find a method called setSecurityManager and tell MySystemInterceptor to
         // intercept it (the method binding is smart enough to take it from there)
         final AgentBuilder.Transformer transformer =
-                (b, typeDescription) -> b.method(ElementMatchers.named("setSecurityManager"))
-                        .intercept(MethodDelegation.to(MySystemInterceptor.class));
+                new AgentBuilder.Transformer() {
+                    @Override
+                    public DynamicType.Builder<?> transform(DynamicType.Builder<?> builder, TypeDescription typeDescription, ClassLoader classLoader) {
+                        return builder.method(ElementMatchers.named("setSecurityManager"))
+                                .intercept(MethodDelegation.to(MySystemInterceptor.class));
+                    }
+                };
 
         // Disable a bunch of stuff and turn on redefine as the only option
         final ByteBuddy byteBuddy = new ByteBuddy().with(Implementation.Context.Disabled.Factory.INSTANCE);
         final AgentBuilder agentBuilder = new AgentBuilder.Default()
-                .withByteBuddy(byteBuddy)
-                .withInitializationStrategy(AgentBuilder.InitializationStrategy.NoOp.INSTANCE)
-                .withRedefinitionStrategy(AgentBuilder.RedefinitionStrategy.REDEFINITION)
-                .withTypeStrategy(AgentBuilder.TypeStrategy.Default.REDEFINE)
+                .with(byteBuddy)
+                .with(AgentBuilder.InitializationStrategy.NoOp.INSTANCE)
+                .with(AgentBuilder.RedefinitionStrategy.REDEFINITION)
+                .with(AgentBuilder.TypeStrategy.Default.REDEFINE)
                 .type(systemType)
                 .transform(transformer);
 

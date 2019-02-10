c4272b212a3e198c8e4149252f22ebd26fe8441d
Pap Lőrinc <pap.lorinc@gmail.com>
2019-02-10 16:54:39 +0100

    Simplify injectBootstrapClasses and pom files (#3)
    * Cleanup pom files
    
    * Simplify injectBootstrapClasses

--- agent/src/main/java/com/tersesystems/securityfixer/agent/SecurityFixerAgent.java
+++ agent/src/main/java/com/tersesystems/securityfixer/agent/SecurityFixerAgent.java
@@ -1,39 +1,33 @@
 package com.tersesystems.securityfixer.agent;
 
-import java.io.*;
-import java.lang.instrument.Instrumentation;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.jar.JarOutputStream;
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.agent.builder.AgentBuilder.InitializationStrategy;
 import net.bytebuddy.agent.builder.AgentBuilder.Listener;
 import net.bytebuddy.agent.builder.AgentBuilder.RedefinitionStrategy;
 import net.bytebuddy.agent.builder.AgentBuilder.TypeStrategy;
 import net.bytebuddy.asm.Advice;
-
-import static java.nio.file.Files.*;
-import static java.nio.file.StandardOpenOption.DELETE_ON_CLOSE;
-import static net.bytebuddy.dynamic.ClassFileLocator.CLASS_FILE_EXTENSION;
-import static net.bytebuddy.matcher.ElementMatchers.*;
+import net.bytebuddy.dynamic.loading.ClassInjector;
 import net.bytebuddy.matcher.StringMatcher;
 
-public class SecurityFixerAgent {
+import java.io.File;
+import java.io.IOException;
+import java.lang.instrument.Instrumentation;
+import java.nio.file.Files;
 
-    private static final List<Class<?>> BOOTSTRAP_CLASSES = Collections.singletonList(
-            MySystemInterceptor.class
-    );
+import static java.util.Collections.singletonMap;
+import static net.bytebuddy.description.type.TypeDescription.ForLoadedType;
+import static net.bytebuddy.dynamic.ClassFileLocator.ForClassLoader.read;
+import static net.bytebuddy.dynamic.loading.ClassInjector.UsingInstrumentation.Target.BOOTSTRAP;
+import static net.bytebuddy.matcher.ElementMatchers.*;
+
+public class SecurityFixerAgent {
+    private static final Class<?> INTERCEPTOR_CLASS = MySystemInterceptor.class;
 
     private SecurityFixerAgent() {
     }
 
     // From https://stackoverflow.com/questions/44747219/byte-buddy-advice-onmethodexit-constructor-retransformation
-    public static void premain(String arg, Instrumentation instrumentation) {
+    public static void premain(String arg, Instrumentation instrumentation) throws Exception {
         injectBootstrapClasses(instrumentation);
         new AgentBuilder.Default()
                 .with(RedefinitionStrategy.RETRANSFORMATION)
@@ -45,41 +39,16 @@ public class SecurityFixerAgent {
                         Listener.StreamWriting.toSystemOut()))
                 .type(named("java.lang.System"))
                 .transform((builder, type, classLoader, module) ->
-                        builder.visit(Advice.to(MySystemInterceptor.class).on(named("setSecurityManager")))
+                        builder.visit(Advice.to(INTERCEPTOR_CLASS).on(named("setSecurityManager")))
                 )
                 .installOn(instrumentation);
     }
 
-    private static void injectBootstrapClasses(Instrumentation instrumentation) {
-        try {
-            Path jarFile = createTempFile(SecurityFixerAgent.class.getSimpleName(), ".jar");
-            jarFile.toFile().deleteOnExit();
-            try (JarOutputStream jarOutputStream = new JarOutputStream(new BufferedOutputStream(newOutputStream(jarFile)))) {
-                for (Class<?> bootstrapClass : BOOTSTRAP_CLASSES) {
-                    String klassPath = classFileFullname(bootstrapClass);
-                    jarOutputStream.putNextEntry(new JarEntry(klassPath));
-                    jarOutputStream.write(readFully(bootstrapClass.getClassLoader().getResourceAsStream(klassPath)));
-                }
-            }
-            instrumentation.appendToBootstrapClassLoaderSearch(new JarFile(jarFile.toFile()));
-        } catch (IOException exception) {
-            throw new IllegalStateException("Cannot write jar file to disk", exception);
-        }
-    }
+    private static void injectBootstrapClasses(Instrumentation instrumentation) throws IOException {
+        File temp = Files.createTempDirectory("tmp").toFile();
+        temp.deleteOnExit();
 
-    private static String classFileFullname(Class<?> bootstrapClass) {
-        return bootstrapClass.getName().replace('.', '/') + CLASS_FILE_EXTENSION;
+        ClassInjector.UsingInstrumentation.of(temp, BOOTSTRAP, instrumentation)
+            .inject(singletonMap(new ForLoadedType(INTERCEPTOR_CLASS), read(INTERCEPTOR_CLASS)));
     }
-
-    private static byte[] readFully(InputStream input) throws IOException {
-        try (ByteArrayOutputStream output = new ByteArrayOutputStream()) {
-            byte[] buffer = new byte[8192];
-            int bytesRead;
-            while ((bytesRead = input.read(buffer)) != -1) {
-                output.write(buffer, 0, bytesRead);
-            }
-            return output.toByteArray();
-        }
-    }
-
 }
